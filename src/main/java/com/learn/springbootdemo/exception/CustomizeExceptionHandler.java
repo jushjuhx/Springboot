1dacfe427c6751b26069f20988fde8c74b3a9b10
mounsurf <mounsurf@users.noreply.github.com>
2020-03-06 11:32:43 +0800

    Update CustomizeExceptionHandler.java

--- src/main/java/com/learn/springbootdemo/exception/CustomizeExceptionHandler.java
+++ src/main/java/com/learn/springbootdemo/exception/CustomizeExceptionHandler.java
@@ -2,6 +2,7 @@ package com.learn.springbootdemo.exception;
 
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,10 +16,15 @@ public class CustomizeExceptionHandler implements HandlerExceptionResolver {
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
                                          Exception ex) {
         String msg = ex.getMessage();
-
         try {
             response.addHeader("Content-Type", "text/html; charset=UTF-8");
-            response.getWriter().append("自定义异常处理!!! \n").append(msg).flush();
+
+            if (ex instanceof NoHandlerFoundException) {
+                //需要配合application.properties中的spring.mvc.throw-exception-if-no-handler-found=true使用
+                response.getWriter().append("自定义异常处理!!! \n").append("找不到路由").flush();
+            } else {
+                response.getWriter().append("自定义异常处理!!! \n").append(msg).flush();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
